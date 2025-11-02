e45932f50fa1bc2c839c0513d518ca8323861587
Callum Rossiter <59575047+Teekiz@users.noreply.github.com>
2025-11-02 12:50:44 +0000

    Updated images.

--- src/app/components/SimulatorUI.tsx
+++ src/app/components/SimulatorUI.tsx
@@ -9,13 +9,13 @@ export default function SimulatorUI()
     const [total, setTotal] = useState(0);
     const [items, setItems] = useState<ItemRow[]>([
         { id: 8, name: "Nexling", quantity: 0, image_url: "src/assets/Nexling.png" },
-        { id: 6, name: "Ancient hilt", quantity: 0, image_url: "src/assets/Ancient_hilt.png" },
-        { id: 2, name: "Nihil Horn", quantity: 0, image_url: "src/assets/Nihil_horn.png" },
-        { id: 1, name: "Zaryte vambraces", quantity: 0, image_url: "src/assets/Zaryte_vambraces.png" },
-        { id: 3, name: "Torva full helm (damaged)", quantity: 0, image_url: "src/assets/Torva_full_helm_(damaged).png" },
-        { id: 4, name: "Torva platebody (damaged)", quantity: 0, image_url: "src/assets/Torva_platebody_(damaged).png" },
-        { id: 5, name: "Torva platelegs (damaged)", quantity: 0, image_url: "src/assets/Torva_platelegs_(damaged).png" },
-        { id: 7, name: "Nihil shard", quantity: 0, image_url: "src/assets/Nihil_shard.png" },
+        { id: 6, name: "Ancient hilt", quantity: 0, image_url: "src/assets/Ancienthilt.png" },
+        { id: 2, name: "Nihil Horn", quantity: 0, image_url: "src/assets/Nihilhorn.png" },
+        { id: 1, name: "Zaryte vambraces", quantity: 0, image_url: "src/assets/Zarytevambraces.png" },
+        { id: 3, name: "Torva full helm (damaged)", quantity: 0, image_url: "src/assets/Torvafullhelmdamaged.png" },
+        { id: 4, name: "Torva platebody (damaged)", quantity: 0, image_url: "src/assets/Torvaplatebodydamaged.png" },
+        { id: 5, name: "Torva platelegs (damaged)", quantity: 0, image_url: "src/assets/Torvaplatelegsdamaged.png" },
+        { id: 7, name: "Nihil shard", quantity: 0, image_url: "src/assets/Nihilshard.png" },
     ]);
 
     return (
