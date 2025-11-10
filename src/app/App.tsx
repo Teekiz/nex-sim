d4612f885a0f7a1436333c07925c030a3d61f6ed
Callum Rossiter <59575047+Teekiz@users.noreply.github.com>
2025-11-10 19:16:49 +0000

    Added title and seperated css files.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,22 @@
-import './App.css'
+import '../css/App.css'
+import '../css/Fonts.css'
+import '../css/Components/CollectionLog.css'
 import InputBox from "./components/Controls/InputBox.tsx";
 import DisplayContainer from "./components/Display/DisplayContainer.tsx";
+import Box from "@mui/material/Box";
+import TitleBox from "./components/Display/Title/TitleBox.tsx";
 
 function App() {
   return (
-      <>
-          <InputBox />
-          <DisplayContainer />
-      </>
+      <Box>
+          <TitleBox></TitleBox>
+          <Box className="outer_app_container">
+              <Box className="inner_app_box">
+                  <InputBox />
+                  <DisplayContainer />
+                </Box>
+          </Box>
+      </Box>
   );
 }
 
---
+++ src/app/components/Display/Title/TitleBox.tsx
@@ -0,0 +1,10 @@
+import Box from "@mui/material/Box";
+
+export default function TitleBox()
+{
+    return (
+        <Box className={"title_box"}>
+            <h1>Nex Loot Simulator</h1>
+        </Box>
+    );
+}
