41573a15795b493bf38745c56d91fdbfbfffccab
Callum Rossiter <59575047+Teekiz@users.noreply.github.com>
2025-11-13 23:41:38 +0000

    Updated theme provider.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,18 +6,59 @@ import InputBox from "./components/Controls/InputBox.tsx";
 import DisplayContainer from "./components/Display/DisplayContainer.tsx";
 import Box from "@mui/material/Box";
 import TitleBox from "./components/Display/Title/TitleBox.tsx";
+import {createTheme, ThemeProvider} from "@mui/material";
 
 function App() {
+
+    const theme = createTheme({
+        palette: {
+            text: {
+                primary: "#ffffff",
+                secondary: "#fff500"
+            }
+        },
+        typography: {
+            fontFamily: 'RunescapePlain',
+            fontSize: 18
+        },
+        components: {
+            MuiOutlinedInput: {
+                styleOverrides: {
+                    root: {
+                        "& fieldset": {
+                            borderColor: "white",
+                        },
+                        "&:hover fieldset": {
+                            borderColor: "lightgray",
+                        },
+                        "&.Mui-focused fieldset": {
+                            borderColor: "white",
+                        },
+                    },
+                },
+            },
+            MuiInputLabel: {
+                styleOverrides: {
+                    root: {
+                        color: "white",
+                    },
+                },
+            },
+        },
+    });
+
   return (
-      <Box>
-          <TitleBox></TitleBox>
-          <Box className="outer_app_container">
-              <Box className="inner_app_box">
-                  <InputBox />
-                  <DisplayContainer />
-                </Box>
+      <ThemeProvider theme={theme}>
+          <Box>
+              <TitleBox></TitleBox>
+              <Box className="outer_app_container">
+                  <Box className="inner_app_box">
+                      <InputBox />
+                      <DisplayContainer />
+                    </Box>
+              </Box>
           </Box>
-      </Box>
+      </ThemeProvider>
   );
 }
 
