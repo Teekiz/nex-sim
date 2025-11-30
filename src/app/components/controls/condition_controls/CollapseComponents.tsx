f4228b7d2320c6f9e93b134b3f8752d5d3667bfa
Callum Rossiter <59575047+Teekiz@users.noreply.github.com>
2025-11-30 17:21:27 +0000

    Updated to use MUIs grid system instead of manually setting sizes (collapse).

--- src/app/components/controls/condition_controls/CollapseComponents.tsx
+++ src/app/components/controls/condition_controls/CollapseComponents.tsx
@@ -1,4 +1,4 @@
-import {Box, Collapse, Stack} from "@mui/material";
+import {Collapse, Stack} from "@mui/material";
 import {Condition} from "../../../../lib/enum/conditions.ts";
 import TextField from "@mui/material/TextField";
 import ItemsCheckbox from "./ItemsCheckbox.tsx";
@@ -13,30 +13,27 @@ interface CollapseComponentsProps {
 
 export default function CollapseComponents({condition, rolls, setRolls, selectedItems, handleItemSelection}: CollapseComponentsProps) {
     return (
-        <Stack useFlexGap={true} width={"100%"} direction="column" alignItems="center" flexWrap={"wrap"} justifyContent={"center"}>
+        <Stack useFlexGap={true} width={{xs: "90%", sm: "70%", md: "50%"}} direction="column" alignItems="center" flexWrap={"wrap"} justifyContent={"center"}>
             {/* Number of rolls input */}
-            <Box sx={{width: 140}}>
-                <Collapse in={condition === Condition.UNTIL_ROLL_COUNT ||
-                    condition === Condition.UNTIL_UNIQUE_COUNT ||
-                    condition === Condition.UNTIL_SHARD_COUNT}>
-                    <TextField
-                        label="Number of Rolls"
-                        variant="filled"
-                        value={rolls}
-                        onChange={(e) => setRolls(Number(e.target.value))}
-                        type={"text"}
-                        inputMode={"numeric"}/>
-                </Collapse>
-            </Box>
+            <Collapse in={condition === Condition.UNTIL_ROLL_COUNT ||
+                condition === Condition.UNTIL_UNIQUE_COUNT ||
+                condition === Condition.UNTIL_SHARD_COUNT}>
+                <TextField
+                    label="Number of Rolls"
+                    variant="filled"
+                    size={"small"}
+                    value={rolls}
+                    onChange={(e) => setRolls(Number(e.target.value))}
+                    type={"text"}
+                    inputMode={"numeric"}/>
+            </Collapse>
 
             {/* Select item input */}
-            <Box sx={{maxWidth: 500}}>
-                <Collapse in={condition === Condition.UNTIL_SELECTED_ITEMS}>
-                    <ItemsCheckbox
-                        handleSelected={handleItemSelection}
-                        selectedIds={selectedItems}/>
-                </Collapse>
-            </Box>
+            <Collapse in={condition === Condition.UNTIL_SELECTED_ITEMS}>
+                <ItemsCheckbox
+                    handleSelected={handleItemSelection}
+                    selectedIds={selectedItems}/>
+            </Collapse>
         </Stack>
     );
 }
